8b8ed240bcd01464b24bccdffe6765c6cae96440
Kazushi Lambert <kazushi.lambert@gmail.com>
2024-12-19 00:17:07 +0900

    Create S1.py

---
+++ Day 5/S1.py
@@ -0,0 +1,10 @@
+"""
+how to do it : 
+cut the document into 2 parts
+turn the first part in to a list containning lists witht the tow number keeping their order
+turn the second part in to a list of list as well
+create a check function that takes ([the update], [the rule]) and returns True if that ruled is being followed and False if not
+Do a double loop that takes all the updates and tests them with all the different rules one by one
+the complexity is horrific but it should work
+"""
+#the answer was
