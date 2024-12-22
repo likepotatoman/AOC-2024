d387ee9e2602236248d79994fababe966fa91fd0
Kazushi Lambert <kazushi.lambert@gmail.com>
2024-12-22 14:49:47 +0900

    Update S1.py

--- Day 6/S1.py
+++ Day 6/S1.py
@@ -1,10 +1,9 @@
-with open("AOC2024 D-6 Input.txt", "r") as file:
+with open("C:/Users/dmlam/Desktop/AOC2024 D-6.txt", "r") as file:
     content = file.read()
     lines = content.splitlines()  # This returns a list of lines, with newline characters
 map = []
 for i in range(len(lines)):
     map.append(list(lines[i]))
-print(map)
 
 def find_starting_point(map):
     for i in range(len(map)):
@@ -12,7 +11,6 @@ def find_starting_point(map):
             if map[i][j] == "^":
                 return [i, j]
 position_garde = find_starting_point(map)
-
 
 def find_obstacle(map):
     obstacles = []
@@ -22,40 +20,38 @@ def find_obstacle(map):
                 obstacles.append([i, j])
     return obstacles
 position_obstacles = find_obstacle(map)
-print(position_obstacles)
 
 
 direction = 0 #direction initale = haut, avec rotation 90d droite pour chaque direction % 4
 anciennes_positions =[]
-while 1 <= position_garde[0] < 128 and 1 <= position_garde[1] < 128 :
-    print(position_garde)
+while 0 <= position_garde[0] < 130 and 0 <= position_garde[1] < 130 :
     if direction % 4 == 0:
         if [position_garde[0]-1, position_garde[1]] not in position_obstacles:
-            anciennes_positions.append(position_garde)
+            if position_garde not in anciennes_positions:
+                anciennes_positions.append(list(position_garde))
             position_garde[0] -= 1
         else :
             direction += 1
     elif direction % 4 == 2:
         if [position_garde[0]+1, position_garde[1]] not in position_obstacles:
-            anciennes_positions.append(position_garde)
+            if position_garde not in anciennes_positions:
+                anciennes_positions.append(list(position_garde))
             position_garde[0] += 1
         else :
             direction += 1
     elif direction % 4 == 1:
         if [position_garde[0], position_garde[1]+1] not in position_obstacles:
-            anciennes_positions.append(position_garde)
+            if position_garde not in anciennes_positions:
+                anciennes_positions.append(list(position_garde))
             position_garde[1] += 1
         else :
             direction += 1
     elif direction % 4 == 3:
         if [position_garde[0], position_garde[1]-1] not in position_obstacles:
-            anciennes_positions.append(position_garde)
+            if position_garde not in anciennes_positions:
+                anciennes_positions.append(list(position_garde))
             position_garde[1] -= 1
         else :
             direction += 1
-
-
-print(anciennes_positions)
-
 print(len(anciennes_positions))
-#the answer was 
+#the answer was 5242
